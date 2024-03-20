4bdbfdc4227ce4223cfac81f56f2dde9f9dd057f
MinhazMurks <MinhazMurks+Github@gmail.com>
2024-03-20 03:15:27 -0400

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,47 @@
+<script>
+	export const prerender = true;
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Minnie Wedding" />
+</svelte:head>
+
+<section>
+	<h1>
+		Jennie + Minhaz
+	</h1>
+</section>
+
+<style>
+    @font-face {
+        font-family: 'Gnomon-Web';
+        font-style: normal;
+        font-weight: 500;
+        src: url('/fonts/gnomon-web.ttf'); /* IE9 Compat Modes */
+        src: local(''),
+            /* Modern Browsers */ url('/fonts/gnomon-web.ttf') format('truetype')
+    }
+
+    @font-face {
+        font-family: 'Gnomon-Simple';
+        font-style: normal;
+        font-weight: 500;
+        src: url('/fonts/gnomon-simple.ttf'); /* IE9 Compat Modes */
+        src: local(''),
+            /* Modern Browsers */ url('/fonts/gnomon-simple.ttf') format('truetype')
+    }
+
+    section {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        flex: 0.6;
+    }
+
+    h1 {
+        font-family: 'Gnomon-Web', serif;
+        font-size: 10em;
+    }
+</style>
---
+++ src/routes/NavBar.svelte
@@ -0,0 +1,87 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<header>
+	<div>
+
+	</div>
+	<nav>
+		<ul>
+			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
+				<a href="/">Home</a>
+			</li>
+			<li aria-current={$page.url.pathname === '/info' ? 'page' : undefined}>
+				<a href="/info">Destination</a>
+			</li>
+			<li aria-current={$page.url.pathname === '/rsvp' ? 'page' : undefined}>
+				<a href="/rsvp">RSVP</a>
+			</li>
+<!--			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>-->
+<!--				<a href="/sverdle">Sverdle</a>-->
+<!--			</li>-->
+		</ul>
+	</nav>
+
+</header>
+
+<style>
+    header {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    nav {
+        display: flex;
+        justify-content: center;
+        width: 100vw;
+        background: rgba(255, 255, 255, 0.7);
+    }
+
+    ul {
+        position: relative;
+        padding: 0;
+        margin: 0;
+        height: 3em;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        list-style: none;
+        background-size: contain;
+    }
+
+    li {
+        position: relative;
+        height: 100%;
+    }
+
+    li[aria-current='page']::before {
+        --size: 6px;
+        content: '';
+        width: 0;
+        height: 0;
+        position: absolute;
+        top: 0;
+        left: calc(50% - var(--size));
+        border: var(--size) solid transparent;
+        border-top: var(--size) solid var(--color-theme-1);
+    }
+
+    nav a {
+        display: flex;
+        height: 100%;
+        align-items: center;
+        padding: 0 0.5rem;
+        color: var(--color-text);
+        font-weight: 700;
+        font-size: 0.8rem;
+        text-transform: uppercase;
+        letter-spacing: 0.1em;
+        text-decoration: none;
+        transition: color 0.2s linear;
+    }
+
+    a:hover {
+        color: var(--color-theme-1);
+    }
+</style>
