938539c39a490e0b5dd938089d41bcdd8c3f976d
MinhazMurks <MinhazMurks+Github@gmail.com>
2024-03-26 01:48:50 -0400

    remove paths base

--- svelte.config.js
+++ svelte.config.js
@@ -12,10 +12,7 @@ const config = {
 			fallback: undefined,
 			precompress: false,
 			strict: true
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
+		})
 	}
 };
 
